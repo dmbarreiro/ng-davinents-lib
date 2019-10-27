851bac9a73f969a814518c981530654deebf8bbd
dmbarreiro <5133990+dmbarreiro@users.noreply.github.com>
2019-10-27 21:07:44 +0100

    Making directive selector specific to components using it

--- projects/ng-davinents-lib/src/lib/common/input-reference.directive.ts
+++ projects/ng-davinents-lib/src/lib/common/input-reference.directive.ts
@@ -2,7 +2,7 @@ import { Directive, HostListener } from "@angular/core";
 
 @Directive({
   // Applies to any input directive inside app-fa-input
-  selector: "app-fa-input input"
+  selector: "[dvts-fa-input input], [dvts-md-input input]"
 })
 export class InputReferenceDirective {
   focus = false;
